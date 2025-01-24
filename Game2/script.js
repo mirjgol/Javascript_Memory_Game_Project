fcbf7469888f8b708dacd94832cd1c9f35f60992
Mirjam Goldiger <goldiger.mirjam@gmail.com>
2025-01-24 11:44:52 +0100

    joker function and time over function

--- Game2/script.js
+++ Game2/script.js
@@ -16,6 +16,7 @@ const scoreTimeWin= document.querySelector(".score-time-win");
 const rulesWindow = document.querySelector(".rules-container");
 const buttonRules = document.querySelector(".rules-button");
 const closeRules = document.querySelector(".close-button");
+const buttonJoker = document.querySelector(".joker-container")
 
 //rules
 
@@ -42,10 +43,29 @@ function startTimer(){
 if (isTimerRunning) return;
 isTimerRunning = true;
 timePassed = 0; // reset passed time
+
 timerInterval = setInterval(() => {
   timePassed++;
   timerContent.innerText = `Time: ${timePassed}s`;
+
+  // nach Ablauf von 120s verliert man das Spiel ebenfalls
+
+  if (timePassed === 120) {
+    timeOver(); // löst Game over Funktion aus
+  }
 },1000);
+
+}
+
+
+// time over funktion, wenn 120 s verstrichen
+
+function timeOver(){
+  stopTimer();
+  looseMessage.classList.add("visible");
+    scoreLoose.innerText = `${fails}`;
+    scoreTimeLoose.innerText = `${timePassed}s`;
+
 }
 
 
@@ -95,7 +115,10 @@ gameOver();
 
 
 
+
 }
+
+
 //check for match , compare cards function
 function checkForMatch() {
   if (firstCard.dataset.background === secondCard.dataset.background 
@@ -108,7 +131,7 @@ function checkForMatch() {
     
     disableCards();
     score += 1;
-     scoreText.innerText = `Score:${score}`;
+     scoreText.innerText = `Cat Pairs:${score}`;
     // updateScore();
     // changeScore():
    
@@ -158,10 +181,10 @@ function gameOver(){
     looseMessage.classList.add("visible");
     scoreLoose.innerText = `${fails}`;
     scoreTimeLoose.innerText = `${timePassed}s`;
-   
-
   }
+
 }
+
 
 
 
@@ -172,7 +195,7 @@ function gameOver(){
 
 function restartGame(){
   resetTimer();
-  
+ 
   score = 0;
   fails = 0;
   hasFlippedCard = false;
@@ -186,13 +209,17 @@ function restartGame(){
   looseMessage.classList.remove("visible");
   winMessage.classList.remove("visible");
 
+// Joker wieder einblenden
+   buttonJoker.classList.remove("hidden");
+
   cards.forEach(card=>{
     card.classList.remove("flip");
     // warum nochmals eventlistener???
     card.addEventListener("click",flipCard);
   });
 
-  shuffle();
+   shuffle();
+
 
 
 }
@@ -207,7 +234,7 @@ function restartGame(){
 
 
 
-// entferne flip(eventListener)bei gefundenen paaren
+// entferne flip card event listener bei gefundenen paaren
 
 function disableCards() {
 
@@ -270,7 +297,30 @@ closeRules.addEventListener("click", (event) => {
 
 cards.forEach(card => card.addEventListener("click", flipCard));
 
+
+// add EventListener to joker and flip all cards click event: 
+
+buttonJoker.addEventListener("click", () => {
+    buttonJoker.classList.add("hidden");
+  cards.forEach(card => card.classList.add("flip"));
+  setTimeout(() =>{
+    cards.forEach(card => card.classList.remove("flip"));
+  }, 2000); // nach 2 sekunden jocker flip cards vorbei
+
+});
+
+// nachdem 1x benutz wurde add class hidden 
+
+
+
+
+
+
+
+
 // squares in the background
+const squareColors = ["#b8348c", "#7e0054", "purple", "plum","rgb(65, 10, 65)"];
+
 let squares = []
 
 class Square {
@@ -278,8 +328,8 @@ class Square {
 constructor(){ // blueprint der properties
   this.x = Math.floor (Math.random()*window.innerWidth);
   this.y = Math.floor (Math.random()*window.innerHeight);
-  this.color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-  this.size = Math.floor(Math.random()*14)
+  this.color = squareColors[Math.floor(Math.random() * squareColors.length)];
+  this.size = Math.floor(Math.random()*16)
 }
 
 show () { // funktion die die squares zeichnen soll
@@ -301,7 +351,7 @@ show () { // funktion die die squares zeichnen soll
 
 
 
-for ( let i = 0; i<100; i++) {
+for ( let i = 0; i<120; i++) {
   squares.push(new Square());
   squares[i].show();
 
